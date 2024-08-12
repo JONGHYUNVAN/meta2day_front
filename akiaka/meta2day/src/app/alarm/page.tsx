7b061ddd60d148fad21c2c5239c9c809fa6d14a0
yoon-gang <sgi07151@naver.com>
2024-08-12 15:39:41 +0900

    refactor.배경 기본값으로 설정

--- akiaka/meta2day/src/app/alarm/page.tsx
+++ akiaka/meta2day/src/app/alarm/page.tsx
@@ -4,7 +4,7 @@ import AlarmList from './alarmList';
 
 const AlarmPage: React.FC = () => {
     return (
-        <div className="flex justify-center items-center min-h-screen bg-blue-100">
+        <div className="flex justify-center items-center min-h-screen">
             <div className="bg-white p-8 rounded-lg shadow-lg w-96">
                 <h1 className="text-2xl font-bold mb-6">알림</h1>
                 <AlarmList />
