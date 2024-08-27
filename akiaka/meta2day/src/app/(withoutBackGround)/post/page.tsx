bea2fa4631e9c0f607db80008593d2f834ed00dd
JONGHYUNVAN <116232652+JONGHYUNVAN@users.noreply.github.com>
2024-08-27 11:05:27 +0900

    Revert "Initial commit"
    This reverts commit 83631a51c6f4716d8c310c8129542d7324734675.

--- akiaka/meta2day/src/app/(withoutBackGround)/post/page.tsx
+++
@@ -1,9 +0,0 @@
-import PostForm from "@/app/(withoutBackGround)/post/pageForm";
-
-export default function PostPage() {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-transparen">
-                <PostForm/>
-        </div>
-    );
-}
