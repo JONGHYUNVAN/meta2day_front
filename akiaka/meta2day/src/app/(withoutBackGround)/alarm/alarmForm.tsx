8997352464cb6e3b6f111e120f9cd8163dfa3721
yoon-gang <sgi07151@naver.com>
2024-08-29 16:17:30 +0900

    feat.알림 스크롤기능

--- akiaka/meta2day/src/app/(withoutBackGround)/alarm/alarmForm.tsx
+++ akiaka/meta2day/src/app/(withoutBackGround)/alarm/alarmForm.tsx
@@ -16,7 +16,6 @@ const AlarmForm: React.FC = () => {
     const [notifications, setNotifications] = useState<Alarm[]>([]);
     const { user } = useAuth();
 
-
     // 서버로부터 알람 목록을 가져오는 함수
     useEffect(() => {
         const fetchNotifications = async () => {
@@ -24,7 +23,6 @@ const AlarmForm: React.FC = () => {
                 const token = localStorage.getItem('token');
                 const nickname = user?.nickname;
                 if (!nickname || !token) return; // nickname이나 token이 없을 경우 early return
-                
 
                 const response = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/alarm/user/${nickname}`, {
                     headers: {
@@ -42,8 +40,8 @@ const AlarmForm: React.FC = () => {
     }, [user]);
 
     return (
-        <div className="h-auto  flex items-center justify-center bg-transparent">
-            <div className="relative mt-48 max-w-6xl w-full font-serif opacity-80 hover:opacity-90 transition-opacity duration-200 shadow-md rounded-lg">
+        <div className="min-h-screen flex items-center justify-center bg-transparent">
+            <div className="relative mt-32 max-w-6xl w-full font-serif opacity-80 hover:opacity-90 transition-opacity duration-200 shadow-md rounded-lg">
                 <div className="metalic-bar absolute top-0 left-0 right-0 h-12 bg-gradient-to-r from-gray-700 via-gray-600 to-gray-700 rounded-t-lg shadow-metallic">
                     <div className="absolute top-2 left-3 flex space-x-2">
                         <span className="w-3 h-3 bg-red-500 rounded-full neon-effect-red"></span>
@@ -51,7 +49,8 @@ const AlarmForm: React.FC = () => {
                         <span className="w-3 h-3 bg-green-500 rounded-full neon-effect-green"></span>
                     </div>
                 </div>
-                <div className="content-container mt-12 bg-[#191919] p-4 rounded-b-lg animate-unfold">
+                
+                <div className="content-container mt-12 bg-[#191919] p-4 rounded-b-lg h-[calc(100%-64px)] overflow-y-auto">
                     <h1 className="text-2xl font-bold mb-6 text-white">알림 목록</h1>
                     {notifications.length > 0 ? (
                         notifications.map((notification) => (
@@ -62,7 +61,7 @@ const AlarmForm: React.FC = () => {
                             </div>
                         ))
                     ) : (
-                            <p className="text-white">알림이 없습니다.</p>
+                        <p className="text-white">알림이 없습니다.</p>
                     )}
                 </div>
             </div>
